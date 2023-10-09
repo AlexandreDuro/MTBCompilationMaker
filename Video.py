ab29a2a361faf1ff9d216ecbcc82a30dea16b109
Alex <alexandreduro11@gmail.com>
2023-10-09 22:12:51 +0200

    Added discord Embed to follow steps

--- Video.py
+++ Video.py
@@ -1,8 +1,9 @@
 import warnings
 from datetime import datetime
 import asyncio
-from rich.console import Console
+import discord
 
+from discord import Embed
 from video_utils import *
 
 console = Console()
@@ -11,45 +12,83 @@ warnings.filterwarnings("ignore")
 
 class Video:
 
-    def __init__(self):
+    def __init__(self, interaction: discord.Interaction = None):
         self.date_and_time = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
         self.video_files = []
         self.converted_files = []
+        self.interaction = interaction
 
     async def build_video(self, urls):
         delete_video_files()
+
+        # Embed initial
+        initial_embed = Embed(title="Building video", description="Initializing...", color=0x00ff00)
+        await self.interaction.edit_original_response(embed=initial_embed)
+
         for i, url in enumerate(urls):
             converted_url = convert_pinkbike_url(url)
             filename = f"Downloaded/video_{i}.mp4"
             self.video_files.append(filename)
 
-            console.print(f"Downloading video {i + 1}/{len(urls)}", style="italic blue")
-            await download_video(converted_url, filename)
-            console.print(f"Successfully downloaded video {i + 1}/{len(urls)}", style="italic green")
+            # Update the embed
+            progress_embed = Embed(title="Building video", description=f"Downloading video {i + 1}/{len(urls)}",
+                                   color=0x00ff00)
+            await self.interaction.edit_original_response(embed=progress_embed)
 
+            # Download the video
+            await download_video(converted_url, filename)
+
+            # Update the embed again
+            success_embed = Embed(title="Building video",
+                                  description=f"Successfully downloaded video {i + 1}/{len(urls)}", color=0x00ff00)
+            await self.interaction.edit_original_response(embed=success_embed)
+
+            # Check if the file is empty
             if os.path.getsize(filename) == 0:
                 raise ValueError(f"The file {filename} is empty!")
 
+            # Get the converted filename
             converted_filename = "Converted/" + get_converted_filename(filename.split("/")[1].split(".")[0] + ".mp4")
-            console.print(f"Converting video {i + 1}/{len(urls)}", style="italic blue")
-            await convert_video(filename, converted_filename)
-            console.print(f"Successfully converted video {i + 1}/{len(urls)}", style="italic green")
 
+            # Convert the video
+            converting_embed = Embed(title="Building video", description=f"Converting video {i + 1}/{len(urls)}",
+                                     color=0x00ff00)
+            await self.interaction.edit_original_response(embed=converting_embed)
+            await convert_video(filename, converted_filename)
+
+            # Successfully converted video
+            converted_embed = Embed(title="Building video",
+                                    description=f"Successfully converted video {i + 1}/{len(urls)}", color=0x00ff00)
+            await self.interaction.edit_original_response(embed=converted_embed)
+
+            # Add the converted file to the list
             self.converted_files.append(converted_filename)
 
-        console.print("Combining the videos...", style="italic blue")
+        # Combining videos
+        combining_embed = Embed(title="Building video", description="Combining the videos...", color=0x00ff00)
+        await self.interaction.edit_original_response(embed=combining_embed)
         combined_clip = concatenate_videos(self.converted_files)
 
-        console.print("Writing the final video...", style="italic blue")
+        # Writing the final video
+        writing_embed = Embed(title="Building video", description="Writing the final video...", color=0x00ff00)
+        await self.interaction.edit_original_response(embed=writing_embed)
         combined_clip.write_videofile("Result/result.mp4", preset="ultrafast", codec="libx264",
                                       ffmpeg_params=['-profile:v', 'high', '-level', '4.2', '-pix_fmt', 'yuv420p',
                                                      '-hide_banner', '-loglevel', 'error'])
 
-        console.print("Adding watermark to the final video...", style="italic blue")
+        # Adding watermark
+        watermark_embed = Embed(title="Building video", description="Adding watermark to the final video...",
+                                color=0x00ff00)
+        await self.interaction.edit_original_response(embed=watermark_embed)
         await add_watermark("Result/result.mp4", "Result/result_watermark.mp4")
 
-        console.print("Adding audio to the final video...", style="italic blue")
+        # Adding audio
+        audio_embed = Embed(title="Building video", description="Adding audio to the final video...", color=0x00ff00)
+        await self.interaction.edit_original_response(embed=audio_embed)
         await add_subscribe_voice("Result/result_watermark.mp4", f"C:/PublicDownloads/result{self.date_and_time}.mp4")
-        console.print("Done!", style="bold green")
-    delete_video_files()
 
+        # Done
+        done_embed = Embed(title="Done!", description="Your video is ready!", color=0x00ff00)
+        await self.interaction.edit_original_response(embed=done_embed)
+
+    delete_video_files()
--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
 import discord
+from discord import Embed
 from datetime import datetime
 from utils import get_download_url
 
@@ -12,15 +13,29 @@ class Commands:
         self._register_commands()
 
     def _register_commands(self):
-        @self.tree.command(name="pinkbike", description="Get a compiled video from pinkbike links", guild=discord.Object(id=self.config.guild_id))
+        @self.tree.command(name="pinkbike", description="Get a compiled video from pinkbike links",
+                           guild=discord.Object(id=self.config.guild_id))
         async def pinkbike(interaction: discord.Interaction, urls: str):
             from Video import Video
-            await interaction.response.send_message("Building video...", ephemeral=True)
-            original_message = await interaction.original_response()
-            video = Video()
+
+            initial_embed = Embed(title="Building video...", description="Your video is being processed.",
+                                  color=0x00ff00)
+            await interaction.response.send_message(embed=initial_embed, ephemeral=True)
+
+            video = Video(interaction=interaction)
             await video.build_video(urls.split(","))
-            await interaction.followup.send("Uploading the video!", ephemeral=True)
+
+            upload_embed = Embed(title="Uploading the video!", description="Your video is being uploaded.",
+                                 color=0x00ff00)
+            await interaction.edit_original_response(embed=upload_embed)
+
             date_and_time = video.date_and_time
             download_url = get_download_url(f"result{date_and_time}.mp4")
-            await interaction.followup.send(download_url)
-            await interaction.followup.send("Video built successfully!", ephemeral=True)
+
+            download_embed = Embed(title="Download Video", description=f"[Click here to download]({download_url})",
+                                   color=0x0000ff)
+            await interaction.followup.send(embed=download_embed)
+
+            success_embed = Embed(title="Success!", description="Your video has been successfully uploaded.",
+                                  color=0x00ff00)
+            await interaction.edit_original_response(embed=success_embed)
